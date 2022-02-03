89a02ef3140291f72fcd7b54bc12605aac02e210
Horjamo Olivia V <hdolivia@cubbli20-gold.local>
2022-02-03 14:17:16 +0200

    Fix errors in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,51 +1,61 @@
-CREATE TABLE users (
+CREATE TABLE employers (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
-	password TEXT,
-	role INTEGER
+	password TEXT
+);
+
+CREATE TABLE employees (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	password TEXT
 );
 
 CREATE TABLE job_experience (
 	id SERIAL PRIMARY KEY,
-	employee_id INTEGER REFERENCES users,
+	employee_id INTEGER REFERENCES employees,
 	employer TEXT,
 	role TEXT,
 	description TEXT,
 	beginning DATE,
-	end DATE
+	ended DATE
 );
 
 CREATE TABLE education (
 	id SERIAL PRIMARY KEY,
-	employee_id INTEGER REFERNCES users,
+	employee_id INTEGER REFERENCES employees,
 	school TEXT,
 	level TEXT,
 	description TEXT,
-	beginning DATE
+	beginning DATE,
 	graduation DATE
 );
 
+CREATE TABLE application_forms (
+        id SERIAL PRIMARY KEY,
+        opened TIMESTAMP,
+        closing DATE,
+        question_1 TEXT,
+        question_2 TEXT,
+        question_3 TEXT,
+        question_4 TEXT,
+        question_5 TEXT
+);
+
+
 CREATE TABLE jobs (
 	id SERIAL PRIMARY KEY,
-	employer_id INTEGER REFERENCES users,
-	employee_id INTEGER REFERENCES users,
+	employer_id INTEGER REFERENCES employers,
+	employee_id INTEGER REFERENCES employees,
 	role TEXT,
 	description TEXT,
 	beginning DATE,
-	end DATE,
+	ends DATE,
 	status INTEGER,
 	visible INTEGER,
-	form INTEGER REFERENCES application_forms
+	form INTEGER REFERENCES application_forms,
 	visible_in_profile INTEGER
 );
 
-CREATE TABLE application_forms (
-	question_1 TEXT,
-	question_2 TEXT,
-	question_3 TEXT,
-	question_4 TEXT,
-	question_5 TEXT,
-);
 
 CREATE TABLE applications (
 	form_id INTEGER REFERENCES application_forms,
@@ -57,4 +67,4 @@ CREATE TABLE applications (
 	answer_5 TEXT,
 	sent_at TIMESTAMP,
 	employee_id INTEGER REFERENCES employees
-)
+);
