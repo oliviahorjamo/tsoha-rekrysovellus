100b219982a4f8acc8dd30e628e052e18b9efb29
Horjamo Olivia V <hdolivia@cubbli20-gold.local>
2022-02-04 10:53:46 +0200

    Fix configuration

--- db.py
+++ db.py
@@ -4,5 +4,5 @@ from os import getenv
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URL"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
