d69dad8706372e74c6dcd125196abac1fefb8994
Horjamo Olivia V <hdolivia@cubbli20-gold.local>
2022-03-05 19:10:48 +0200

    Change the deleting of a profile text
    Previously all profile texts were stored in the database. Now
    the previous profile text is deleted when a new one is created.

--- profiles.py
+++ profiles.py
@@ -1,18 +1,12 @@
 
 from db import db
 
-#profiilitekstin muokkaaminen tehdään lähettämällä uusi profiiliteksti ja poistamalla sen jälkeen vanha jos onnistuu
-
 def add_profile_text(user_id, text):
     """adds profile text to the profile (both employees and employers)"""
+    sql = "INSERT into PROFILE_TEXT (profile_text, user_id, visible) VALUES (:text, :user_id, 1)"
+    db.session.execute(sql, {"text":text, "user_id":user_id})
+    db.session.commit()
     try:
-        sql = "UPDATE PROFILE_TEXT SET visible = 0 WHERE user_id=:user_id"
-        db.session.execute(sql, {"user_id":user_id})
-        db.session.commit()
-        #tähän kohtaan pitäisi laittaa vanhan profiilitekstin poistaminen, tavallaan ei haittaa jos käyttäjällä useita profiilitekstejä, mutta
-        #jotenkin olis hyvä pitää tiedossa mikä on viimeisin
-        #esim alkuun vois laittaa vanhojen profiilitekstien laittamisen piiloon
-        #jos haluaa kokonaan poistaa niin voi poistaa tän jälkeen ne missä visible = 0
         sql = "INSERT into PROFILE_TEXT (profile_text, user_id, visible) VALUES (:text, :user_id, 1)"
         db.session.execute(sql, {"text":text, "user_id":user_id})
         db.session.commit()
@@ -20,19 +14,21 @@ def add_profile_text(user_id, text):
     except:
         return False
 
-#tätä funktiota ei varmaan tarvita, koska käyttäjä voi "poistaa" profiilitekstinsä lähettämällä vaan tyhjän lomakkeen
-#def delete_profile_text(user_id):
- #   """deletes a profile text"""
-  #  sql = "UPDATE PROFILE_TEXT SET profile_text = NULL where user_id =:user_id"
-   # db.session.execute(sql, {"user_id": user_id})
-    #db.session.commit()
-
-
 def get_profile_text(user_id):
     """returns the profile text of a given user"""
     try:
         sql = "SELECT profile_text from PROFILE_TEXT where user_id =:user_id and visible = 1"
         return db.session.execute(sql, {"user_id": user_id}).fetchone()[0]
+    except:
+        return False
+
+def delete_profile_text(user_id):
+    """deletes the profile text of the given user"""
+    try:
+        sql = "UPDATE PROFILE_TEXT SET visible = 0 WHERE user_id=:user_id"
+        db.session.execute(sql, {"user_id":user_id})
+        db.session.commit()
+        return True
     except:
         return False
 
